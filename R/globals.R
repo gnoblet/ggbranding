bdb6018011b6ecf7b49207221e7ceb51eb2853dc
gnoblet <gnoblet@zaclys.net>
2025-09-20 19:04:32 +0200

    add icons_df as a global variable

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+# Global variable declarations to suppress R CMD check notes
+# These variables are either:
+# - Package data objects (icons_df)
+
+# Declare package data object
+utils::globalVariables("icons_df")
